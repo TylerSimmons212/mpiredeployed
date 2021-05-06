b4b4a6ffdca2509644f87c770ee460117f3d2575
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2021-05-06 17:19:37 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import './App.css';
 import Nav from './Components/Nav/Nav';
 import Hero from './Components/Hero/Hero';
 import UsvOthers from './Components/UsvOthers/Usvothers';
+import Capabilities from './Components/OurCapabilities/Capabilities';
 
 
 function App() {
   return (
     <div className="App">
     {/* Nav */}
-    <Nav />
+     <Nav />
     {/* Hero */}
     <Hero />
     {/* Brands */}
-    
     {/* Us VS Other */}
     <UsvOthers />
+    {/* About US */}
+    {/* Brands Results */}
+    {/* Our Capabilities */}
+    <Capabilities />
+    {/* Footer */}
     </div>
   );
 }
