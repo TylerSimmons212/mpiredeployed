bac0928ba8bdef4969b0739e51d5146ddca35ce7
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2021-05-11 17:21:26 -0600

    Update Usvothers.js

--- src/Components/UsvOthers/Usvothers.js
+++ src/Components/UsvOthers/Usvothers.js
@@ -198,7 +198,7 @@ export default function FullWidthTabs() {
                     variant="contained"
                     color="primary"
                     onClick={handleReset}
-                    className={classes.button}
+                    className="buttonnext"
                   >
                     Reset
                   </Button>: 
@@ -206,7 +206,7 @@ export default function FullWidthTabs() {
                     variant="contained"
                     color="primary"
                     onClick={handleNext}
-                    className={classes.button}
+                    className="buttonnext"
                   >
                     Next
                   </Button>}
@@ -224,99 +224,13 @@ export default function FullWidthTabs() {
           </Button>
         </Paper>
       )}
-        {/* <AppBar position="static" id="appbar">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            className="tabs"
-            variant="fullWidth"
-            aria-label="full width tabs example"
-          >
-            <Tab label="Agencies Are Pricey" {...a11yProps(0)} />
-            <Tab label="In House Is Pricey" {...a11yProps(1)} />
-            <Tab label="Transparency" {...a11yProps(2)} />
-            <Tab label="Competitive" {...a11yProps(3)} />
-            <Tab label="Our Pricing" {...a11yProps(4)} />
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-          <TabPanel value={value} index={0} dir={theme.direction}>
-            <h1>1. Hiring an agency is expensive.</h1>
-            <p>Agencies typically charge a percentage of ad spend, usually 10-20% with steep minimums to pay their employees.<br/><br/>Agencies typically only assign one or two junior/mid level experienced employees to your account. All the high level experienced guys get promoted to director level positions and are usually not hands on in your accounts.</p>
-            <Button variant="contained" href="#contained-buttons" className="contactbtntxt">
-                  Work With Us Instead
-            </Button>
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-          <h1>2. Hiring in-house is expensive.</h1>
-            <p>Hiring a truly valuable experienced ad buyer would cost you a MINIMUM $80k salary with all the employee paperwork, insurance, onboarding headaches, and everything else that comes with it. 
-            ‍<br/><br/>
-            When you hire us brothers, you get multiple highly experienced advertising brothers for less than the cost of ONE in house employee with a simple signed agreement and a 1099. Lose the headaches.</p>
-            <Button variant="contained" href="#contained-buttons" className="contactbtntxt">
-                  Work With Us Instead
-            </Button>
-          </TabPanel>
-          <TabPanel value={value} index={2} dir={theme.direction}>
-          <h1>3. Transparency is more valuable than an ongoing sales pitch.</h1>
-            <p>We take pride in our relationships as 90% of our clientele has come to us through client referrals. 
-            ‍<br/><br/>
-            We always struggled working with agencies that tried to beat around the bush to keep a client thinking things are going well so they can keep you spending money on them.
-            We prefer being upfront about issues and bringing solutions. We have a reputation for being fair with those we work with.
-            <br/><br/>
-            Our relationships need to be mutually beneficial to create lasting relationships and most importantly, friendships. It feels less like work when we help our friends. </p>
-            <Button variant="contained" href="#contained-buttons" className="contactbtntxt">
-                  Let's Chat
-            </Button>
-          </TabPanel>
-          <TabPanel value={value} index={3} dir={theme.direction}>
-          <h1>4. Competitive<br/><span className="subtitle">(only pay for the services we are currently providing).</span></h1>
-            <p>We have a cost effective solution to delivering your needs. We have an a la cart system in the way we charge. We offer a wide range of skill sets outside of just ad buying to ensure success with scaling your brand. 
-            ‍<br/><br/>
-            If you want us to only manage Facebook Ads, we will only charge for that. The more work we offer, the less you pay per service delivered. This gives you full control of what you want to be paying for, saving you a lot of money. This helps us clearly understand our position in the relationship with your brand. 
-            <br/><br/>
-            Though we love to be open about ideas on how to better scale your brand for success.
-            </p>
-            <Button variant="contained" href="#contained-buttons" className="contactbtntxt">
-                  Let's Chat
-            </Button>
-          </TabPanel>
-          <TabPanel value={value} index={4} dir={theme.direction}>
-          <h1>5. Our Pricing Structure</h1>
-            <div className="pricingboxes">
-              <div className="pricingbox">
-                <h2>$1,000/Mo</h2>
-                <p>- Creative Work (with expectations) - SMS Management</p>
-                <p>- Consulting (1hr/week) $250/hr</p>
-              </div>
-              <div className="pricingbox">
-                <h2>$3,000/Mo</h2>
-                <p>Single Platform Ad Management<br/>(eg. Facebook)</p>
-              </div>
-              <div className="pricingbox">
-                <h2>$4,000/Mo</h2>
-                <p>- 2 Platform Ad Management<br/>(eg. Facebook, Google)</p>
-              </div>
-              <div className="pricingbox">
-                <h2>$5,000/Mo</h2>
-                <p>- 3+ Platform Ad Management<br/>(eg. Facebook, Google, Pinterest)</p>
-              </div>
-            </div>
-            <div className="pricingboxbottom">
-                <h2>One-Time Pay</h2>
-                <p>- Audits (Each Ad Account $500)</p>
-                <p>- Campaign buildout ($3000 / full build + consulting = $4000)</p>
-            </div>
-            <Button variant="contained" href="#contained-buttons" className="contactbtntxt">
-                  Let's Chat
-            </Button>
-          </TabPanel>
-        </SwipeableViews> */}
+      
       </div>
+      <div className="contactbtn">
+            <Button variant="contained" href="mailto: braden@mpire.media" className="contactbtntxt2">
+                  WORK WITH US
+            </Button>
+        </div>
     </div>
   );
 }
