da7e385ddc976c833400830e470e0891449b15f5
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2021-05-11 19:42:05 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import './App.css';
-import Nav from './Components/Nav/Nav';
-import Hero from './Components/Hero/Hero';
-import Brands2 from './Components/Brands/Brands2';
-import UsvOthers from './Components/UsvOthers/Usvothers';
-import Capabilities from './Components/OurCapabilities/Capabilities';
-import Footer from './Components/Footer/Footer';
-import AboutUs from './Components/AboutUs/Aboutus';
-import Results from './Components/BrandsResults/Results2';
+import Home from './Components/Home/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-    {/* Nav */}
-     <Nav />
-    {/* Hero */}
-    <Hero />
-    {/* Brands */}
-    <Brands2 />
-    {/* Us VS Other */}
-    <UsvOthers />
-    {/* About US */}
-    <AboutUs />
-    {/* Brands Results */}
-    <Results />
-    {/* Our Capabilities */}
-    <Capabilities />
-    {/* Footer */}
-    <Footer />
+      <Home />
     </div>
   );
 }
