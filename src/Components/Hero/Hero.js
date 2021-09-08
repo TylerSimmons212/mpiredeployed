425659475316df4f2a2435cf8eacca10c8f24ad9
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2021-09-08 15:44:30 -0600

    Update Hero.js

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -18,7 +18,7 @@ const Hero = (props) => {
 
     return ( 
     <div className = 'hero' >
-        <h2>We Aren't An Ad Agency,<br/> We Are An Extension Of Your Team.</h2>
+        <h2>Think of us as an extension of your team.</h2>
         <p>We also happen to be brothers with a reputation of over $300m in trackable sales collectively.</p>
         <div className="contactbtn">
             <Button variant="contained" onClick={() => { props.setForm(true) }} className="contactbtntxt2">
