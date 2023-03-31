8b504dd746a8aa3ddd19ac201363ad3b2058f727
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2023-03-31 10:57:45 -0700

    Update Creative Teams to remove REVI, Matix, and Chamber and now include Studiio

--- src/Components/TeamsWeWorkWith/TeamsWeWorkWith.js
+++ src/Components/TeamsWeWorkWith/TeamsWeWorkWith.js
@@ -1,10 +1,8 @@
 import React from "react";
-import chamber from "../../images/teamslogos/chambermedialogo.png";
 import creatably from "../../images/teamslogos/creatablylogo.png";
 import filmlaab from "../../images/teamslogos/filmlaab.png";
-import matixlabs from "../../images/teamslogos/matixlabslogo.png";
-import revi from "../../images/teamslogos/revilogo.png";
 import sipstudios from "../../images/teamslogos/sipostudioslogo.png";
+import studiio from "../../images/teamslogos/STUDIIO.png";
 
 import "./teams.css";
 
@@ -14,23 +12,17 @@ const Teams = () => {
       <div className="teams-content">
         <h2>CREATIVE TEAMS WE WORK WITH</h2>
         <div class="container">
-          <div class="wrapper" id="chamber">
-            <img src={chamber} alt="Chamber Media" />
-          </div>
           <div class="wrapper" id="creatably">
             <img src={creatably} alt="Creatably" />
           </div>
           <div class="wrapper" id="filmlaab">
             <img src={filmlaab} alt="Film Laab" />
           </div>
-          <div class="wrapper" id="matix">
-            <img src={matixlabs} alt="Matix Labs" />
-          </div>
-          <div class="wrapper" id="revi">
-            <img src={revi} alt="Revi" />
-          </div>
           <div class="wrapper" id="sipo">
             <img src={sipstudios} alt="Sipo Studios" />
+          </div>
+          <div class="wrapper" id="studiio">
+            <img src={studiio} alt="STUDIIO" />
           </div>
         </div>
       </div>
