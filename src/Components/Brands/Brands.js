34e6ddaa601b8e4db6b8a536f5dc16851db2e981
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2023-04-04 11:18:11 -0700

    Update Logo Conveyer Belt to use images from CDN

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -1,198 +1,171 @@
 import React from "react";
 import "./brands.css";
-import transparentlabs from "../../images/brandlogos/transparentlabslogo.png";
-import sleepgram from "../../images/brandlogos/sleepgramlogo.png";
-import buckedup from "../../images/brandlogos/buckedup.png";
-import mrcool from "../../images/brandlogos/mrcool.png";
-import jackedfactory from "../../images/brandlogos/jackedfactorylogo.png";
-import credit101logo from "../../images/brandlogos/credit101logo.png";
-import shine from "../../images/brandlogos/shinelogo.png";
-import aeromixer from "../../images/brandlogos/aeromixerlogo.png";
-import goodranchers from "../../images/brandlogos/good-ranchers.png";
-import reinventu from "../../images/brandlogos/reinvent-u.png";
-import sonix from "../../images/brandlogos/sonix.gif";
-import tuned from "../../images/brandlogos/tuned.png";
-import waveblock from "../../images/brandlogos/waveblock.png";
-import drip from "../../images/brandlogos/dripzlogo.png";
-import fairkind from "../../images/brandlogos/fairkind.png";
-import leadgenesis from "../../images/brandlogos/lead-genesis.png";
-import mixhers from "../../images/brandlogos/mixherslogo.png";
-import natures from "../../images/brandlogos/natureslogo.png";
-import rookie from "../../images/brandlogos/rookie.png";
-import saalt from "../../images/brandlogos/saaltlogo.png";
-import senya from "../../images/brandlogos/senya.png";
-import vanessa from "../../images/brandlogos/vanessa.png";
-import tamarak from "../../images/brandlogos/tamaraklogo.png";
-import projectsolar from "../../images/brandlogos/projectsolarlogo.png";
-import hypo from "../../images/brandlogos/hypologo.png";
-import peachyinc from "../../images/brandlogos/peachyinclogo.png";
-import spoonful from "../../images/brandlogos/spoonfullogo.png";
 
 const Brands2 = () => {
   return (
     <div class="slider">
       <div class="slide-track">
         <div class="slide">
-          <img src={transparentlabs} alt="transparentlabs" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftransparentlabslogo.png" alt="transparentlabs" />
         </div>
         <div class="slide">
-          <img src={drip} alt="drip" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fdripzlogo.png" alt="drip" />
         </div>
         <div class="slide">
-          <img src={aeromixer} alt="aeromixer" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Faeromixerlogo.png" alt="aeromixer" />
         </div>
         <div class="slide">
-          <img src={goodranchers} alt="goodranchers" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fgood-ranchers.png" alt="goodranchers" />
         </div>
         <div class="slide">
-          <img src={reinventu} alt="reinventu" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Freinvent-u.png" alt="reinventu" />
         </div>
         <div class="slide">
-          <img src={sonix} alt="sonix" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsonix.gif" alt="sonix" />
         </div>
         <div class="slide">
-          <img src={tuned} alt="tuned" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftuned.png" alt="tuned" />
         </div>
         <div class="slide">
-          <img src={waveblock} alt="waveblock" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fwaveblock.png" alt="waveblock" />
         </div>
         <div class="slide">
-          <img src={buckedup} alt="buckedup" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fbuckedup.png" alt="buckedup" />
         </div>
         <div class="slide">
-          <img src={sleepgram} alt="sleepgram" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsleepgramlogo.png" alt="sleepgram" />
         </div>
         <div class="slide">
-          <img src={jackedfactory} alt="jackedfactory" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fjackedfactorylogo.png" alt="jackedfactory" />
         </div>
         <div class="slide">
-          <img src={projectsolar} alt="projectsolar" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fprojectsolarlogo.png" alt="projectsolar" />
         </div>
         <div class="slide">
-          <img src={mrcool} alt="mrcool" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fmrcool.png" alt="mrcool" />
         </div>
         <div class="slide">
-          <img src={credit101logo} alt="credit101logo" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fcredit101logo.png" alt="credit101logo" />
         </div>
         <div class="slide">
-          <img src={shine} alt="shine" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fshinelogo.png" alt="shine" />
         </div>
         <div class="slide">
-          <img src={fairkind} alt="fairkind" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ffairkind.png" alt="fairkind" />
         </div>
         <div class="slide">
-          <img src={leadgenesis} alt="leadgenesis" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Flead-genesis.png" alt="leadgenesis" />
         </div>
         <div class="slide">
-          <img src={mixhers} alt="mixhers" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fmixherslogo.png" alt="mixhers" />
         </div>
         <div class="slide">
-          <img src={natures} alt="natures" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fnatureslogo.png" alt="natures" />
         </div>
         <div class="slide">
-          <img src={rookie} alt="rookie" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Frookie.png" alt="rookie" />
         </div>
         <div class="slide">
-          <img src={saalt} alt="saalt" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsaaltlogo.png" alt="saalt" />
         </div>
         <div class="slide">
-          <img src={senya} alt="senya" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsenya.png" alt="senya" />
         </div>
         <div class="slide">
-          <img src={tamarak} alt="tamarak" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftamaraklogo.png" alt="tamarak" />
         </div>
         <div class="slide">
-          <img src={vanessa} alt="vanessa" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fvanessa.png" alt="vanessa" />
         </div>
         <div class="slide">
-          <img src={hypo} alt="Hypo Chlorous Company" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fhypologo.png" alt="Hypo Chlorous Company" />
         </div>
         <div class="slide">
-          <img src={peachyinc} alt="Peachy Inc" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fpeachyinclogo.png" alt="Peachy Inc" />
         </div>
         <div class="slide">
-          <img src={spoonful} alt="Spoonful of Comfort" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fspoonfullogo.png" alt="Spoonful of Comfort" />
         </div>
         <div class="slide">
-          <img src={transparentlabs} alt="transparentlabs" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftransparentlabslogo.png" alt="transparentlabs" />
         </div>
         <div class="slide">
-          <img src={drip} alt="drip" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fdripzlogo.png" alt="drip" />
         </div>
         <div class="slide">
-          <img src={aeromixer} alt="aeromixer" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Faeromixerlogo.png" alt="aeromixer" />
         </div>
         <div class="slide">
-          <img src={goodranchers} alt="goodranchers" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fgood-ranchers.png" alt="goodranchers" />
         </div>
         <div class="slide">
-          <img src={reinventu} alt="reinventu" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Freinvent-u.png" alt="reinventu" />
         </div>
         <div class="slide">
-          <img src={sonix} alt="sonix" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsonix.gif" alt="sonix" />
         </div>
         <div class="slide">
-          <img src={tuned} alt="tuned" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftuned.png" alt="tuned" />
         </div>
         <div class="slide">
-          <img src={waveblock} alt="waveblock" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fwaveblock.png" alt="waveblock" />
         </div>
         <div class="slide">
-          <img src={buckedup} alt="buckedup" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fbuckedup.png" alt="buckedup" />
         </div>
         <div class="slide">
-          <img src={sleepgram} alt="sleepgram" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsleepgramlogo.png" alt="sleepgram" />
         </div>
         <div class="slide">
-          <img src={jackedfactory} alt="jackedfactory" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fjackedfactorylogo.png" alt="jackedfactory" />
         </div>
         <div class="slide">
-          <img src={projectsolar} alt="projectsolar" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fprojectsolarlogo.png" alt="projectsolar" />
         </div>
         <div class="slide">
-          <img src={mrcool} alt="mrcool" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fmrcool.png" alt="mrcool" />
         </div>
         <div class="slide">
-          <img src={credit101logo} alt="credit101logo" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fcredit101logo.png" alt="credit101logo" />
         </div>
         <div class="slide">
-          <img src={shine} alt="shine" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fshinelogo.png" alt="shine" />
         </div>
         <div class="slide">
-          <img src={fairkind} alt="fairkind" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ffairkind.png" alt="fairkind" />
         </div>
         <div class="slide">
-          <img src={leadgenesis} alt="leadgenesis" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Flead-genesis.png" alt="leadgenesis" />
         </div>
         <div class="slide">
-          <img src={mixhers} alt="mixhers" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fmixherslogo.png" alt="mixhers" />
         </div>
         <div class="slide">
-          <img src={natures} alt="natures" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fnatureslogo.png" alt="natures" />
         </div>
         <div class="slide">
-          <img src={rookie} alt="rookie" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Frookie.png" alt="rookie" />
         </div>
         <div class="slide">
-          <img src={saalt} alt="saalt" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsaaltlogo.png" alt="saalt" />
         </div>
         <div class="slide">
-          <img src={senya} alt="senya" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fsenya.png" alt="senya" />
         </div>
         <div class="slide">
-          <img src={tamarak} alt="tamarak" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Ftamaraklogo.png" alt="tamarak" />
         </div>
         <div class="slide">
-          <img src={vanessa} alt="vanessa" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fvanessa.png" alt="vanessa" />
         </div>
         <div class="slide">
-          <img src={hypo} alt="Hypo Chlorous Company" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fhypologo.png" alt="Hypo Chlorous Company" />
         </div>
         <div class="slide">
-          <img src={peachyinc} alt="Peachy Inc" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fpeachyinclogo.png" alt="Peachy Inc" />
         </div>
         <div class="slide">
-          <img src={spoonful} alt="Spoonful of Comfort" />
+          <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/brand_logos%2Fspoonfullogo.png" alt="Spoonful of Comfort" />
         </div>
       </div>
     </div>
