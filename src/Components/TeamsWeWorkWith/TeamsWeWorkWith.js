1270dd1567624d4d73a5930f8918b95a1c96bbe7
TylerSimmons212 <44686684+TylerSimmons212@users.noreply.github.com>
2023-04-04 11:24:40 -0700

    Update Creative Teams to use CDN images

--- src/Components/TeamsWeWorkWith/TeamsWeWorkWith.js
+++ src/Components/TeamsWeWorkWith/TeamsWeWorkWith.js
@@ -1,9 +1,4 @@
 import React from "react";
-import creatably from "../../images/teamslogos/creatablylogo.png";
-import filmlaab from "../../images/teamslogos/filmlaab.png";
-import sipstudios from "../../images/teamslogos/sipostudioslogo.png";
-import studiio from "../../images/teamslogos/STUDIIO.png";
-
 import "./teams.css";
 
 const Teams = () => {
@@ -13,16 +8,16 @@ const Teams = () => {
         <h2>CREATIVE TEAMS WE WORK WITH</h2>
         <div class="container">
           <div class="wrapper" id="creatably">
-            <img src={creatably} alt="Creatably" />
-          </div>
-          <div class="wrapper" id="studiio">
-            <img src={studiio} alt="STUDIIO" />
+            <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/creative_teams%2Fcreatablylogo.png" alt="Creatably" />
           </div>
           <div class="wrapper" id="filmlaab">
-            <img src={filmlaab} alt="Film Laab" />
+            <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/creative_teams%2Ffilmlaab.png" alt="Film Laab" />
           </div>
           <div class="wrapper" id="sipo">
-            <img src={sipstudios} alt="Sipo Studios" />
+            <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/creative_teams%2Fsipostudioslogo.png" alt="Sipo Studios" />
+          </div>
+          <div class="wrapper" id="studiio">
+            <img src="https://mpiremedia.nyc3.cdn.digitaloceanspaces.com/creative_teams%2FSTUDIIO.png" alt="STUDIIO" />
           </div>
         </div>
       </div>
